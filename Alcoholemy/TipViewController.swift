83da36f051a2c3bb782cb8546a9d500390126fce
mcseves <mariacarolina@MacBook-Pro-de-Maria-2.local>
2015-07-22 12:33:01 -0300

    teste
    loucura no storyboard

---
+++ Alcoholemy/TipViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TipViewController.swift
+//  Alcoholemy
+//
+//  Created by Maria Carolina Santos on 22/07/15.
+//  Copyright (c) 2015 Julia Motta Baitelli. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "Dicas"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
